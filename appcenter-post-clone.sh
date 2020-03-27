ae892ca0c36dd89ecd383e9218af07e964b31786
hadiyanto.hanasil <hadiyanto.hanasil@photoninfotech.net>
2020-03-27 16:56:13 +0700

    fix post clone build scripts

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Example: Install App Center CLI
+cat /etc/os-release
 npm install -g appcenter-cli
 curl -O https://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/3.2.0.1227/sonar-scanner-cli-3.2.0.1227-linux.zip
 unzip sonar-scanner-cli-3.2.0.1227-linux.zip
