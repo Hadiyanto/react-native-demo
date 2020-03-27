96887940b6717fb5a14d79d2de9dacc4a5a3e553
hadiyanto.hanasil <hadiyanto.hanasil@photoninfotech.net>
2020-03-27 16:54:06 +0700

    fix post clone build scripts

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -7,4 +7,5 @@ unzip sonar-scanner-cli-3.2.0.1227-linux.zip
 ls
 cd sonar-scanner-3.2.0.1227-linux/bin
 ls
-sonar-scanner --help
+./sonar-scanner --help
+sonar-scanner-3.2.0.1227-linux/bin/sonar-scanner
